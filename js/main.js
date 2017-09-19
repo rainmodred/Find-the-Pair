942b42b74daf08dff85e1a2e6cc9456cf8b1fd69
rainmodred <albphoenix@gmail.com>
2017-09-19 14:38:26 +0300

    some js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 class Game {
   constructor() {
+    this.cardsGrid = ['moon', 'new-moon', 'goat', 'rat', 'summer', 'winter', 'rabbit', 'star', 'moon', 'new-moon', 'goat', 'rat', 'summer', 'winter', 'rabbit', 'star'];
+
+  }
+
+  newGame() {
+    this.cardsGrid.sort((a, b) => {
+      return 0.5 - Math.random();
+    });
+  }
+
+  render() {
 
   }
 }
 
-var test = document.getElementsByClassName('card')[0];
-console.dir(test);
 
-test.addEventListener('click', (event)=> {
-  
-})
+
+
+const game = new Game();
+game.newGame();
+console.log(game);
+
+const cards = Array.from(document.querySelectorAll('.card'));
+cards.forEach((card,index) => {
+  card.addEventListener('click', () => {
+    card.classList.remove('quest');
+    card.classList.add(game.cardsGrid[index]);
+  })
+});
+
